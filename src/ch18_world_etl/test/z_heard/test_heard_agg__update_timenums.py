9d37869c772c8d015f3a036545639b97e728bee7
Joachim <jschalk@users.noreply.github.com>
2026-03-14 19:22:37 -0600

    fix: convert test to SQLTEST standards #1079

--- src/ch18_world_etl/test/z_heard/test_heard_agg__update_timenums.py
+++ src/ch18_world_etl/test/z_heard/test_heard_agg__update_timenums.py
@@ -2,17 +2,10 @@ from sqlite3 import Cursor
 from src.ch13_time.epoch_main import DEFAULT_EPOCH_LENGTH, get_c400_constants
 from src.ch13_time.test._util.ch13_examples import Ch13ExampleStrs as wx
 from src.ch18_world_etl.etl_sqlstr import (
-    create_prime_tablename as prime_tbl,
     create_sound_and_heard_tables,
     update_heard_agg_timenum_columns,
 )
 from src.ch18_world_etl.test._util.ch18_env import cursor0
-from src.ch18_world_etl.test._util.ch18_examples import (
-    insert_mmtoffi_special_offi_time_otx as insert_offi_time_otx,
-    insert_mmtunit_special_c400_number as insert_c400_number,
-    insert_nabtime_h_agg_otx_inx_time as insert_otx_inx_time,
-    select_mmtoffi_special_offi_time_inx as select_offi_time_inx,
-)
 from src.ch18_world_etl.test.z_heard.test_heard_agg__update_casenum_pchapx import (
     pchapx_insert_nabtime,
     pchapx_insert_prncase,
@@ -25,36 +18,48 @@ from src.ch18_world_etl.test.z_heard.test_heard_agg__update_factnum_pfhapx impor
     pfhapx_insert_prnplan,
     pfhapx_select_prnfact,
 )
+from src.ch18_world_etl.test.z_heard.test_heard_agg__update_moment_time_mxhap0 import (
+    mxhap0_insert_mmtoffi,
+    mxhap0_insert_mmtunit,
+    mxhap0_insert_nabtime,
+    mxhap0_select_mmtoffi,
+)
 from src.ref.keywords import Ch18Keywords as kw, ExampleStrs as exx
 
 
-# TODO convert to bubba testing method once mxhap0 tests are complete
-def test_update_heard_agg_timenum_columns_UpdatesDB_Scenario0_TwoRecordsAndDoesModularMath(
+def test_update_heard_agg_timenum_columns_SQLTEST_Scenario0_TwoRecordsAndDoesModularMath(
     cursor0: Cursor,
 ):
     # ESTABLISH
-    spark1 = 1
-    spark3 = 3
+    create_sound_and_heard_tables(cursor0)
+    spark1, spark3 = (1, 3)
+    s1_otx_time, s1_inx_time = (44, 55)
+    s3_otx_time, s3_inx_time = (400, 550)
+    s1_offi_time_otx, s3_offi_time_otx = (200, 2000)
     a23_c400_number = 5
     a23_epoch_length = get_c400_constants().c400_leap_length * a23_c400_number
     print(f"{a23_epoch_length=}")
     print(f"{DEFAULT_EPOCH_LENGTH=}")
-    s1_otx_time = 44
-    s1_inx_time = 55
-    s1_offi_time_otx = 200
-    s3_otx_time = 400 + a23_epoch_length
-    s3_inx_time = 550
-    s3_offi_time_otx = 2000
+    s3_otx_time, s3_inx_time = (400 + a23_epoch_length, 550)
 
-    create_sound_and_heard_tables(cursor0)
-    insert_c400_number(cursor0, spark1, exx.sue, exx.a23, a23_c400_number)
-    insert_otx_inx_time(cursor0, spark1, exx.sue, exx.a23, s1_otx_time, s1_inx_time)
-    insert_otx_inx_time(cursor0, spark3, exx.sue, exx.a23, s3_otx_time, s3_inx_time)
-    insert_offi_time_otx(cursor0, spark1, exx.sue, exx.a23, s1_offi_time_otx)
-    insert_offi_time_otx(cursor0, spark3, exx.sue, exx.a23, s3_offi_time_otx)
-    mmtoffi_h_agg_tablename = prime_tbl(kw.moment_timeoffi, "h", "agg")
-    assert select_offi_time_inx(cursor0, spark1, exx.a23)[0][3] is None
-    assert select_offi_time_inx(cursor0, spark3, exx.a23)[0][3] is None
+    mmtunit_vals = [[spark1, exx.a23, a23_c400_number]]
+    mxhap0_insert_mmtunit(cursor0, mmtunit_vals)
+    nabtime_vals = [
+        [spark1, exx.a23, s1_otx_time, s1_inx_time],
+        [spark3, exx.a23, s3_otx_time, s3_inx_time],
+    ]
+    mxhap0_insert_nabtime(cursor0, nabtime_vals)
+    mmtoffi_vals = [
+        [spark1, exx.a23, s1_offi_time_otx],
+        [spark3, exx.a23, s3_offi_time_otx],
+    ]
+    mxhap0_insert_mmtoffi(cursor0, mmtoffi_vals)
+
+    # BEFORE
+    assert mxhap0_select_mmtoffi(cursor0, True) == [
+        (1, exx.a23, 200, None),
+        (3, exx.a23, 2000, None),
+    ]
 
     # WHEN
     update_heard_agg_timenum_columns(cursor0)
@@ -64,10 +69,14 @@ def test_update_heard_agg_timenum_columns_UpdatesDB_Scenario0_TwoRecordsAndDoesM
     s3_inx_epoch_diff = s3_otx_time - s3_inx_time
     s1_offi_time_inx = s1_offi_time_otx + s1_inx_epoch_diff
     s3_offi_time_inx = (s3_offi_time_otx + s3_inx_epoch_diff) % a23_epoch_length
-    assert select_offi_time_inx(cursor0, spark1, exx.a23)[0][3] == s1_offi_time_inx
-    assert select_offi_time_inx(cursor0, spark3, exx.a23)[0][3] == s3_offi_time_inx
-    assert select_offi_time_inx(cursor0, spark1, exx.a23)[0][3] == 189
-    assert select_offi_time_inx(cursor0, spark3, exx.a23)[0][3] == 1850
+    assert mxhap0_select_mmtoffi(cursor0, True) == [
+        (1, exx.a23, 200, s1_offi_time_inx),
+        (3, exx.a23, 2000, s3_offi_time_inx),
+    ]
+    assert mxhap0_select_mmtoffi(cursor0, True) == [
+        (1, exx.a23, 200, 189),
+        (3, exx.a23, 2000, 1850),
+    ]
 
 
 def test_update_heard_agg_timenum_columns_SQLTEST_Scenario1_CaseUnit_TimeNums(
